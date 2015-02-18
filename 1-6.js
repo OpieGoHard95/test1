16a97bf8375b0365384f47cf3fdc99fea0f7f835
OpieGoHard95 <jsmith2@saintjoe.edu>
2015-02-18 13:06:33 -0500

    Tried an easier path to the problem for time managment reasons

--- 1-6.js
+++ 1-6.js
@@ -2,10 +2,16 @@
 // Problem 6 from the Javascript 101 set
 // 2/18/15
 
-var caps = function(stringone, stringtwo, stringthree, stringfour) {
-	stringone = stringone.toUpperCase();
-	stringtwo = stringtwo.toUpperCase();
-	stringthree = stringthree.toUpperCase();
-	stringfour = stringfour.toUpperCase();
-	}	
-console.log(caps("hi", "my", "name", "is Jared"));
+// Defines a variable add and specifies that it is a function
+var add = function(x, y, t, v) {
+
+// Gives the function a task to complete in this case adding arguments
+	return x + y + t + v;
+	}
+
+
+
+
+
+// calls the function and gives it arguments to take and add	
+console.log(add(1, 2, 3, 4));
