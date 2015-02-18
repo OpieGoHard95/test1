f4b2ef45e339c688e4fe9efd756e0197a8af31e8
OpieGoHard95 <jsmith2@saintjoe.edu>
2015-02-18 12:49:07 -0500

    Did the problem, changing the variable using an if statement

---
+++ 1-1.js
@@ -0,0 +1,9 @@
+// Coded by Jared Smith
+// Problem number One from the JavaScript 101 set
+// 2/18/15
+
+var i = 22;
+if (i <= 40)
+  i = "i was less than less than or equal to 40!";
+
+console.log(i);
