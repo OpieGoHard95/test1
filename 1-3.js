b70f6ae0a59d249cbb81162c6e8f315a5788ab72
OpieGoHard95 <jsmith2@saintjoe.edu>
2015-02-18 13:18:07 -0500

    Not getting anything to come up not sure why. Here is my attempt though.

---
+++ 1-3.js
@@ -0,0 +1,8 @@
+// Coded by Jared Smith
+// Problem 3 from the JavaScript 101 set
+// 2/18/15
+
+var items = ["stuff", "more stuff", "useless stuff", "important stuff"];
+for (var i = 0; i == items.length; i++)
+	console.log(items.indexOf(i));
+
