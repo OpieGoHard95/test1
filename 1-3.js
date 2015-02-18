a0623b5d8dd86391f6d4c3a0b9d1e78857998dce
OpieGoHard95 <jsmith2@saintjoe.edu>
2015-02-18 13:28:32 -0500

    Forgot to extensively comment my code

--- 1-3.js
+++ 1-3.js
@@ -2,7 +2,10 @@
 // Problem 3 from the JavaScript 101 set
 // 2/18/15
 
+// Creates an array and populates it
 var items = ["stuff", "more stuff", "useless stuff", "important stuff"];
+// Creates a loop that should print out each item one at a time
 for (var i = 0; i == items.length; i++)
 	console.log(items.indexOf(i));
-
+// Problem had to of come from the .indexOf
+// I'm not sure that I am using it correctly
