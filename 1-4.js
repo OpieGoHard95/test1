23a6ce58f1ba9d4a896879cff8ed02bdb2ad11cd
OpieGoHard95 <jsmith2@saintjoe.edu>
2015-02-18 13:27:00 -0500

    Problem 1-4 from the test

---
+++ 1-4.js
@@ -0,0 +1,15 @@
+// Coded by Jared Smith
+// Problem 4 from the Javascript 101 set
+// 2/18/15
+
+var foo = 22;
+// Defines a variable foo and gives it the value of 22
+
+var change = function() {
+	var foo = "not 22";
+// Creates a function that creates a new variable named foo and assigns it a
+// different value
+
+
+console.log(foo);
+// Calls a print of foo which demonstrates scope because foo doesn't change
