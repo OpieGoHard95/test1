44cb18518a95ea6a4bc1e168635f7896681186ed
OpieGoHard95 <jsmith2@saintjoe.edu>
2015-02-18 12:51:30 -0500

    Needed to comment better my grade depends on it

--- 1-1.js
+++ 1-1.js
@@ -2,8 +2,14 @@
 // Problem number One from the JavaScript 101 set
 // 2/18/15
 
+// Defines the variable i and sets it's value to 22
 var i = 22;
+
+// Runs a test to see if i is less than or equal to 40
 if (i <= 40)
+
+// Sets i's value to a message if it was less than or equal to 40
   i = "i was less than less than or equal to 40!";
 
+// Prints out i with either the value 22 or the message depending on the if
 console.log(i);
