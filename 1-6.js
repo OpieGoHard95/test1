a4a702e5572a6a58cce6f6ef787e7cd69e5b3262
OpieGoHard95 <jsmith2@saintjoe.edu>
2015-02-18 13:01:15 -0500

    First attempt at the problem not working, bout to scrap and try again

---
+++ 1-6.js
@@ -0,0 +1,11 @@
+// Coded by Jared Smith
+// Problem 6 from the Javascript 101 set
+// 2/18/15
+
+var caps = function(stringone, stringtwo, stringthree, stringfour) {
+	stringone = stringone.toUpperCase();
+	stringtwo = stringtwo.toUpperCase();
+	stringthree = stringthree.toUpperCase();
+	stringfour = stringfour.toUpperCase();
+	}	
+console.log(caps("hi", "my", "name", "is Jared"));
